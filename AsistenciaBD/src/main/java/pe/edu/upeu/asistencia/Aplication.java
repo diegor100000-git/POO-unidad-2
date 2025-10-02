ac2b8a781458b47fd456fad5481443e50a8a266e
DanielaLis14 <daniela.lisbet14@gmail.com>
2025-10-02 10:52:10 -0500

    web arreglar

--- AsistenciaBD/src/main/java/pe/edu/upeu/asistencia/Aplication.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.asistencia;
-
-public class Aplication {
-
-    public static void main(String[] args) {
-        System.out.println("iniciando asistencia");
-        AsistenciaApplication.main(args);
-    }
-}
