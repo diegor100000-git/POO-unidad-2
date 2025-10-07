12f7fc9df167bcbaadb0344f3bf7d9b6901479f0
DanielaLis14 <daniela.lisbet14@gmail.com>
2025-10-07 09:05:30 -0500

    sisventas

---
+++ SysVentas_/SysVentas_/src/main/java/pe/edu/upeu/sysventas_/SysVentasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysventas_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysVentasApplication.class, args);
+	}
+
+}
---
+++ SysVentas_/SysVentas_/src/test/java/pe/edu/upeu/sysventas_/SysVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysventas_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
